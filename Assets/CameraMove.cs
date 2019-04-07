0dc926b6d65f98b2d66cf4f9b87cc5a5f3940ab5
Vitaly Mayorga <jvitaly.93@gmail.com>
2019-04-07 16:36:33 -0600

    first commit

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    GameObject playerObj;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+         playerObj = GameObject.Find("Tiger");
+         offset = new Vector3(12,9,5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerObj.transform.position + offset;
+    }
+}
---
+++ Assets/TigerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TigerController : MonoBehaviour
+{
+    Vector3 targetPosition;
+    Vector3 lookAtTarget;
+    Quaternion playerRot;
+    float rotSpeed = 3;
+    float speed = 2;
+    bool moving = false;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SetTargetPosition();
+        }
+        if (moving)
+        {
+            Move();
+        }
+        else
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+
+    void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            targetPosition = hit.point;
+            //this.transform.LookAt(targetPosition);
+            lookAtTarget = new Vector3(targetPosition.x - transform.position.x, transform.position.y,
+                targetPosition.z - transform.position.z);
+            playerRot = Quaternion.LookRotation(lookAtTarget);
+            moving = true;
+        }
+    }
+
+    void Move()
+    {
+        if (transform.position != targetPosition)
+        {
+            anim.SetTrigger("Move");
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, rotSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        
+        if(transform.position == targetPosition)
+        {
+            anim.SetTrigger("Idle");
+            moving = false;
+        }
+    }
+}
